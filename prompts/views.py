7bb0c356a246d8ed983a1feeacefc7ce7ce8ba60
setoudie <setoudie@gmail.com>
2024-08-20 15:25:59 +0000

    connection test

--- prompts/views.py
+++ prompts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from pymongo import MongoClient
 # Create your views here.
+
+client = MongoClient('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000')
+
+db = client.neuraldb
+
+people = db.people
+
+people.insert_one({"name": "Seny", "age":24})
+people.insert_one( {"name": "Toutou", "age": 24, "comp": ["Python", "Java"]} )
+
+for person in people.find():
+    print(person)
