09d1894221db0a5c1adf35ee0995c41094544294
setoudie <setoudie@gmail.com>
2024-08-21 08:34:23 +0000

    connection test 001

--- prompts/views.py
+++ prompts/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from pymongo import MongoClient
 # Create your views here.
 
-client = MongoClient('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000')
-
-db = client.neuraldb
+# client = MongoClient('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000')
+client = MongoClient()
+db = client.neurall
 
 people = db.people
 
