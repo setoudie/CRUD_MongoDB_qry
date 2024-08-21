4b7c7391d8fa8cf6447bd2c64fc3f5ad54628e1b
setoudie <setoudie@gmail.com>
2024-08-21 13:25:19 +0000

    connection test 001

--- prompts/views.py
+++ prompts/views.py
@@ -1,15 +1,29 @@
+from traceback import print_tb
+
 from django.shortcuts import render
 from pymongo import MongoClient
 # Create your views here.
 
 # client = MongoClient('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000')
-client = MongoClient()
-db = client.neurall
 
-people = db.people
+def create_prompt(request):
+    client = MongoClient("mongodb://localhost:27017/") # Driver pour creer des db Mongos
+    db = client.promptsdb
+    prompts = db.prompts
 
-people.insert_one({"name": "Seny", "age":24})
-people.insert_one( {"name": "Toutou", "age": 24, "comp": ["Python", "Java"]} )
+    if request.method == 'POST':
+        # print('test')
+        # Creation de la collection `prompts`
+        data = request.POST
+        owner_username = data.get('owner_username')
+        prompt_content = data.get('content')
 
-for person in people.find():
-    print(person)
+        prompt_doc = {'user':f"{owner_username}", "content": f"{prompt_content}"}
+        # print(prompt_doc)
+        try:
+            # insert les information dans la collection `prompts`
+            prompts.insert_one(prompt_doc)
+        except Exception as e:
+            print(f"Erreur : {e}")
+        # return redirect('index', prompt_id=prompt.pid)
+    return render(request, 'prompts/create_prompt.html')
