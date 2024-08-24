3555e904b12485a39ad3472cd2f00f1ac80319fb
setoudie <setoudie@gmail.com>
2024-08-24 22:56:52 +0000

    delete doc with temp ook

--- prompts/urls.py
+++ prompts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create', views.create_prompt, name='create_prompt'),
-    path('delete/<str:prompt_id>', views.delete_prompt, name='delete_prompt'),
+    path('create/', views.create_prompt, name='create_prompt'),
+    path('delete/<str:prompt_id>/', views.delete_prompt, name='delete_prompt'),
+    path('dashboard/', views.show_all_prompt, name='show_all_prompt')
 ]
--- prompts/views.py
+++ prompts/views.py
@@ -1,6 +1,10 @@
-from binascii import a2b_uu
-from http.client import HTTPResponse
-from traceback import print_tb
+"""
+    Objectif : Creer une app CRUD en utilisant une base de donnnes NoSQL de type document (MongoDB ici)
+    C (Create) --> create_prompt(request) : avec cette fonction on arrive a inserer des prompts dans la DB
+    R (Read) --> show_all_prompt(request) : On arrive a visualiser tous les prompts de la DB sur une interface
+    U (Update) --> update_prompt(request, prompt_id) : On doit pouvoir modifier n'importe quel prompt en connaissant l'id
+    D (Delete) --> delete_promt(request, prompt_id) : ```````````````` supprimer ```````````````````````````````````l'id
+"""
 
 from bson.objectid import ObjectId
 from django.http import HttpResponse
@@ -10,16 +14,20 @@ from pymongo import MongoClient
 
 
 # client = MongoClient('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000')
-client = MongoClient("mongodb://localhost:27017/") # Driver pour creer des db Mongos
+client = MongoClient("mongodb://localhost:27017/") # Connection a la DB Mongo
 db = client.promptsdb
 prompts = db.prompts
 
 # Function pour creer un nouveau prompt
+"""
+    Cette fonction est celui qui permet de creer un prompt
+    Elle recupere les informations d'un form html et cree un document (prompt) dans la collection prompts. 
+    Ce document est stocke dans une base de donnes MongoDB (promptsdb) qui une BD NoSQL de type document.
+"""
 def create_prompt(request):
     if request.method == 'POST':
         # print('test')
         # Creation de la collection `prompts`
-        global cmptr
         data = request.POST
         owner_username = data.get('owner_username')
         prompt_content = data.get('content')
@@ -29,15 +37,6 @@ def create_prompt(request):
         try:
             # insert les information dans la collection `prompts`
             prompts.insert_one(prompt_doc)
-
-            # prompt_id = '66c5e8cc13491d3f2bdb91cc'
-            # prompt = db.prompts.find_one({'_id': ObjectId(prompt_id)})
-            #
-            # if prompt:
-            #     print(prompt)
-            # else:
-            #     print("Prompt not found.")
-
         except Exception as e:
             print(f"Erreur : {e}")
         # return redirect('index', prompt_id=prompt.pid)
@@ -46,15 +45,34 @@ def create_prompt(request):
 
 
 # This function is used to delete a specific prompt
+"""
+    Dans cette fonction a partir de l'Id du prompt, on recupere les informations relatifs au prompt a supprimer.
+    Une fois ces informations recuperees on cree un dict ou on les stockent. 
+    Au niveau de mon template il y a un bouton supprimer (action "POST"), ce bouton supprime et redirige vers la page 
+    de c reation de prompt.
+"""
 def delete_prompt(request, prompt_id):
     # Trouver et vérifier si le prompt existe
-    print(prompt_id)
-    prompt_to_delete = db.prompts.find_one({'_id': ObjectId(prompt_id)})
+    # print('1 --> ',prompt_id)
+    id_of_prompt_to_delete = {'_id': ObjectId(prompt_id)}
+    prompt_to_delete = db.prompts.find_one(id_of_prompt_to_delete)
 
-    if not prompt_to_delete:
-        return HttpResponse("Prompt not found", status=404)
+    # prompt_info = [val for val in prompt_to_delete.values()]
+    prompt_content = {
+        'id': prompt_id,
+        'owner_username': prompt_to_delete['user'],
+        'content': prompt_to_delete['content']
+    }
 
-    # Supprimer le document
-    db.prompts.delete_one({'_id': ObjectId(prompt_id)})
+    # print('2 --> ', prompt_content)
 
-    return HttpResponse("good")
+    # print('3 --> ', type(prompt_to_delete), prompt_to_delete['content'])
+
+    if request.method == 'POST':
+        db.prompts.delete_one(id_of_prompt_to_delete) # Delete the prompt
+        return redirect('create_prompt')
+
+    return render(request, 'prompts/delete_prompt.html', {'prompt_to_delete':prompt_content})
+
+def show_all_prompt(request):
+    return HttpResponse('<h1>Here we\'ll show all prompt</h1>')
