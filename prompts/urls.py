7e4c63db918309b01b5ce73fa26374b5acab000e
setoudie <setoudie@gmail.com>
2024-08-24 17:21:33 +0000

    delete doc without temp ook

--- prompts/urls.py
+++ prompts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('create', views.create_prompt, name='create_prompt'),
+    path('delete/<str:prompt_id>', views.delete_prompt, name='delete_prompt'),
 ]
--- prompts/views.py
+++ prompts/views.py
@@ -1,12 +1,13 @@
 from binascii import a2b_uu
+from http.client import HTTPResponse
 from traceback import print_tb
 
-from django.shortcuts import render
+from bson.objectid import ObjectId
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from pymongo import MongoClient
 # Create your views here.
-cmptr = -1
-def auto_id(i):
-    return i+1
+
 
 # client = MongoClient('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000')
 client = MongoClient("mongodb://localhost:27017/") # Driver pour creer des db Mongos
@@ -22,22 +23,38 @@ def create_prompt(request):
         data = request.POST
         owner_username = data.get('owner_username')
         prompt_content = data.get('content')
-        cmptr = cmptr+1
-        print(cmptr)
 
         prompt_doc = {'user':f"{owner_username}", "content": f"{prompt_content}"}
-        print(prompt_doc)
+        # print(prompt_doc)
         try:
             # insert les information dans la collection `prompts`
-            prompts.insert_one(prompt_doc).insert_id
+            prompts.insert_one(prompt_doc)
+
+            # prompt_id = '66c5e8cc13491d3f2bdb91cc'
+            # prompt = db.prompts.find_one({'_id': ObjectId(prompt_id)})
+            #
+            # if prompt:
+            #     print(prompt)
+            # else:
+            #     print("Prompt not found.")
+
         except Exception as e:
             print(f"Erreur : {e}")
         # return redirect('index', prompt_id=prompt.pid)
     return render(request, 'prompts/create_prompt.html')
 
-def delete_prompt(request, prompt_id):
-#     prompts.find_one(objInstance)
-    print(prompt_id)
 
-    if request.method == 'POST':
-        data = request.POST
+
+# This function is used to delete a specific prompt
+def delete_prompt(request, prompt_id):
+    # Trouver et vérifier si le prompt existe
+    print(prompt_id)
+    prompt_to_delete = db.prompts.find_one({'_id': ObjectId(prompt_id)})
+
+    if not prompt_to_delete:
+        return HttpResponse("Prompt not found", status=404)
+
+    # Supprimer le document
+    db.prompts.delete_one({'_id': ObjectId(prompt_id)})
+
+    return HttpResponse("good")
